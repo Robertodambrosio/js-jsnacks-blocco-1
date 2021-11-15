3b257f0d1d097c2727c22c35b0e7e5885721fd3d
Roberto D'Ambrosio <roberto8601@gmail.com>
2021-11-15 16:03:26 +0100

    jsnack3

--- js/script.js
+++ js/script.js
@@ -34,16 +34,16 @@
 // Verificare tramite una funzione che le due parole abbiano la stessa lunghezza.
 // Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
 
- const word1 = prompt("Inserisci una parola");
- const word2 = prompt("Inserisci una parola");
+//  const word1 = prompt("Inserisci una parola");
+//  const word2 = prompt("Inserisci una parola");
 
- if (word1.length == word2.length) {
-     console.log("Le parole hanno la stessa lunghezza: " + word1 + " " + word2)
- } else if (word1.length > word2.length) {
-     console.log("La parola più lunga è: " + word1);
- } else {
-     console.log("La parola più lunga è: " + word2);
- }
+//  if (word1.length == word2.length) {
+//      console.log("Le parole hanno la stessa lunghezza: " + word1 + " " + word2)
+//  } else if (word1.length > word2.length) {
+//      console.log("La parola più lunga è: " + word1);
+//  } else {
+//      console.log("La parola più lunga è: " + word2);
+//  }
 
 
 
@@ -51,10 +51,10 @@
 // Stampa le potenze di 2 fino a 1000.
 
 
-// let pow = 1;
-// while (pow < 1000) {
-// console.log(pow);
-// pow = pow * 2;
-// }
+ let pow = 1;
+ while (pow < 1000) {
+ console.log(pow);
+ pow = pow * 2;
+ }
 
 
