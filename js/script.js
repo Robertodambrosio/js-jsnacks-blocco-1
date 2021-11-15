cb8dcfe9267e38a7bde18bce8029665dd6dbad64
Roberto D'Ambrosio <roberto8601@gmail.com>
2021-11-15 14:46:27 +0100

    JSNACK1

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// ============================JSnack 1
+// Il software deve chiedere per 5 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+// Esegui questo programma in due versioni, con il for e con il while
+
+
+// =========CON IL FOR 
+// let somma = 0;
+
+// for (let i = 0; i < 5; i++){
+//     let numero = parseInt(prompt("inserisci un numero"));
+//     console.log(numero);
+//     somma += numero;
+// }
+//     console.log("La somma dei numeri inseriti è " + somma);
+
+// =========CON IL WHILE
+
+// let i = 0;
+// let somma = 0;
+
+// while (i < 5){
+//     let numero = parseInt(prompt("inserisci un numero"));
+//     console.log(numero);
+//     somma += numero;
+//     i++
+// }
+//     console.log("La somma dei numeri inseriti è " + somma);
+
+
+
+// ==============================JSnack 2
+// Dare la possibilità di inserire due parole.
+// Verificare tramite una funzione che le due parole abbiano la stessa lunghezza.
+// Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+
+
+
+
+
+
+
+//=============================== JSnack 3
+// Stampa le potenze di 2 fino a 1000.
